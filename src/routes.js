de62c477b0f17dc1bc9bc9c72ee12de45a5a46fd
GiulianoMarrocco <giumbles2@gmail.com>
2026-07-30 11:57:41 -0300

    Configura backend, migrations e seed funcionando

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,46 @@ import OrderController from './app/controllers/OrderController.js';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Gerenciamento de usuários
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Cadastra um novo usuário
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: João Silva
+ *               email:
+ *                 type: string
+ *                 example: joao@email.com
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso.
+ *       400:
+ *         description: Dados inválidos.
+ */
+
 //Rotas publicas
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
